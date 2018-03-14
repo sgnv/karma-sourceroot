de78932d8a1923e796c7513b47021dd3c9ee786f
Sirinan Gannigathananon <sirinan.gannigathananon@thomsonreuters.com>
2018-03-14 10:52:01 +0700

    Fix bug, ready to use.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 
-var createSoureRootPreprocessor = function (args, logger) {
+var createSoureRootPreprocessor = function () {
     return function (content, file, done) {
-        file.sourceMap.sourceRoot = file.sourceMap.sourceRoot || file.path.substring(0, file.path.lastIndexOf('/'));
+        if (file && file.sourceMap && file.path) {
+            file.sourceMap.sourceRoot = file.sourceMap.sourceRoot || file.path.substring(0, file.path.lastIndexOf('/'));
+        }
+        done(content);
     };
 };
 
 module.exports = {
-    'preprocessor:sourcemap': ['factory', createSoureRootPreprocessor]
+    'preprocessor:create-sourceroot': ['factory', createSoureRootPreprocessor]
 };
